beeca7402ac2766f5dd07fa7858789b0d2d9fde1
jacquessham <jacquessham2015@gmail.com>
2021-05-21 20:20:22 -0700

    Fix constructor and addelement to function
    Add arr_stats to keep track count and position for helping helper methods

--- ch2/ex6_myarray/myarray.rb
+++ ch2/ex6_myarray/myarray.rb
@@ -1,7 +1,8 @@
 class MyArray
 	def initialize(arr=[], size=20)
 		@size_incre = size
-		@arr = Array.new(size)
+		@arr = Array.new(size) # Real array
+		@arr_stats = {} # Keep track elements
 		@arr_size = 0 # how many elems in array
 		@arr_maxsize = size # physical size of array
 
@@ -12,12 +13,24 @@ class MyArray
 				for elem in arr do
 					# Add integers
 					if is_int(elem)
-						then @arr << Integer(elem)
-							 @arr_size += 1
+						then # Add element to physical array
+							@arr[@arr_size] = Integer(elem)
+							# Check if this elem has a profile in @arr_size
+							if @arr_stats.include?(elem)
+								then # Add info if so
+									@arr_stats[elem]["count"] += 1
+									@arr_stats[elem]["position"] << @arr_size
+								else
+									@arr_stats[elem] = {}
+									@arr_stats[elem]["count"] = 1
+									@arr_stats[elem]["position"] = [@arr_size]
+							end # end if
+							@arr_size += 1
 					else elem_notint << elem
 					end # end if
+
 					# Expand physical size if full
-					if @arr.size == @arr_maxsize
+					if @arr_size == @arr_maxsize
 						then temp = Array.new(@size_incre)
 							 @arr.concat(temp)
 					end # end if	
@@ -41,12 +54,24 @@ class MyArray
 	def addElement(elem)
 		if is_int(elem)
 			then
-				@arr << Integer(elem)
+				@arr[@arr_size] = Integer(elem)
+				# Check if this elem has a profile in @arr_size
+				if @arr_stats.include?(elem)
+					then # Add info if so
+						@arr_stats[elem]["count"] += 1
+						@arr_stats[elem]["position"] << @arr_size
+					else
+						@arr_stats[elem] = {}
+						@arr_stats[elem]["count"] = 1
+						@arr_stats[elem]["position"] = [@arr_size]
+				end # end if
 				if @arr.size == @arr_maxsize
 						then temp = Array.new(@size_incre)
 							 @arr.concat(temp)
 				end # end if
-		else return false
+		else 
+			puts "This element #{elem} you gave is not a integer..."
+			return false
 		end # end if
 		return true
 	end # end method
@@ -58,8 +83,21 @@ class MyArray
 		puts
 	end # end method
 
+	def print_phycial_array
+		for elem in @arr do	
+			if elem != nil then print "#{elem} " else print "nil " end
+		end # end for
+	end
+
+	def print_stats(elem)
+		puts @arr_stats[elem]
+	end
+
 end # end class
 
 # For testing
-a = MyArray.new([1,2,3])
+a = MyArray.new(1..21)
+a.addElement(1)
 a.print_array
+a.print_phycial_array
+a.print_stats(1)
